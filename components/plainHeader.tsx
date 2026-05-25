b67377c9b6a15d3c4e0c85c905de3a1acc3d7390
Samuele <pgn.samu@gmail.com>
2026-05-25 15:18:02 +0200

    new header for coming soon page, but probably it is useless

---
+++ components/plainHeader.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+
+export default function PlainHeader() {
+
+  const primaryColor: string = "black"; // Colore primario
+  const secondaryColor: string = "white"; // Colore secondario
+
+  const router = useRouter();
+
+  return (
+    <>
+      {/* NAVBAR */}
+      <header className="fixed left-0 top-0 z-50 w-full bg-transparent">
+        <nav className="relative flex h-20 items-center justify-between px-6 md:px-10">
+          
+
+          {/* CENTRO - LOGO / NOME SITO */}
+          <div className="absolute left-1/2 -translate-x-1/2">
+            <h1 className={`text-lg font-semibold tracking-wide text-${primaryColor} md:text-xl`}>
+              <a onClick={() => router.push("/")} className="cursor-pointer">
+                Nome Sito
+              </a>
+            </h1>
+          </div>
+
+          
+        </nav>
+      </header>
+    </>
+  );
+}
